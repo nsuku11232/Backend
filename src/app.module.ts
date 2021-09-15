4106f0f166f06da633f3a33d3fd46f597f129e1a
Loraine <gauta388@gmail.com>
2021-09-15 11:41:35 +0200

    my database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://nsuku:nsukuuBee@cluster0.ook0d.mongodb.net/phonebookBackend?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
